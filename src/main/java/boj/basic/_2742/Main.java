036c2a1270ea258b34cb16f18d91cc9dacf27871
sangwon lee <231rollo@gmail.com>
2023-02-01 23:08:04 +0900

    2439 / 2742 / 2884

---
+++ src/main/java/boj/basic/_2742/Main.java
@@ -0,0 +1,17 @@
+package boj.basic._2742;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(N - i);
+        }
+    }
+
+}
